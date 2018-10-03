488e37db59290f5f0af72d5806f3aa6839d6041f
Florent Béjina <florent@bejina.fr>
2018-10-03 15:42:23 +0200

    update createUser

--- src/socketActions/updateUser.js
+++ src/socketActions/updateUser.js
@@ -1,10 +1,12 @@
 import User from '../data/user';
 import formatUser from '../utils/formatUser';
+import SocketManager from '../utils/SocketManager';
 
 const updateUser = socket => async (data) => {
   console.log('[EVENT] on update_user', data);
   const user = await User.update(socket.user._id, data);
-  socket.emit('user_updated', formatUser(user));
+
+  SocketManager.getSocketsByUserId(socket.user._id).forEach(s => s.emit('user_updated', formatUser(user)));
 };
 
 export default updateUser;
