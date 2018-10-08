54445d550b56089e175959f1f08f2bf2154b0c6e
Florent Béjina <florent@bejina.fr>
2018-10-08 15:54:11 +0200

    add nb messages limit on format room

--- src/utils/formatRoom.js
+++ src/utils/formatRoom.js
@@ -3,12 +3,20 @@ import Message from '../data/message';
 import formatUser from './formatUser';
 import formatMessage from './formatMessage';
 
-const formatRoom = async (room) => {
+const formatRoom = async (room, options = {}) => {
   const formatedRoom = Object.assign(room);
 
   formatedRoom.room_id = room._id;
 
-  const messages = await Message.getByIds(formatedRoom.messages)
+  let messagesIds = [];
+
+  if (options && options.nbMessages) {
+    messagesIds = formatedRoom.messages.slice(0, options.nbMessages);
+  } else {
+    messagesIds = formatedRoom.messages;
+  }
+
+  const messages = await Message.getByIds(messagesIds)
     .then(msgs => Promise.all(msgs.map(msg => formatMessage(msg))));
   const users = await User.getByIds(room.users)
     .then(usrs => Promise.all(usrs.map(u => formatUser(u))));
