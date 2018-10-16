32ad96519ea911eb967b60ff8acdb1073e83e104
Florent Béjina <florent@bejina.fr>
2018-10-16 11:13:49 +0200

    update fromatMessage

--- kitchat-server/src/utils/formatMessage.js
+++ kitchat-server/src/utils/formatMessage.js
@@ -2,18 +2,17 @@ import User from '../data/user';
 import formatUser from './formatUser';
 
 const formatMessage = async (message) => {
-  const formatedMessage = Object.assign(message);
+  const formatedMessage = Object.assign({}, message);
 
   formatedMessage.message_id = message._id;
   formatedMessage.user_id = message.user;
   formatedMessage.room_id = message.room;
-  delete message.user;
-  delete message.__v;
-  delete message._id;
+  delete formatedMessage.__v;
+  delete formatedMessage._id;
 
   const user = await User.getById(formatedMessage.user_id).then(u => formatUser(u));
   formatedMessage.user = user;
-  delete message.user_id;
+  delete formatedMessage.user_id;
 
   return {
     ...message,
