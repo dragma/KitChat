8fe13f5b3eb886e8f20b89634210ac651638b49e
Florent Béjina <florent@bejina.fr>
2018-10-16 11:09:11 +0200

    formatUser bugfix

--- kitchat-server/src/utils/formatUser.js
+++ kitchat-server/src/utils/formatUser.js
@@ -1,14 +1,14 @@
 import SocketManager from './SocketManager';
 
 const formatUser = (user) => {
-  const formatedUser = Object.assign(user);
+  const formatedUser = Object.assign({}, user);
 
   formatedUser.kitchat_user_id = user._id;
-  delete user._id;
-  delete user.__v;
+  delete formatedUser._id;
+  delete formatedUser.__v;
   if (user.secondary_id) {
     formatedUser.user_id = user.secondary_id;
-    delete user.secondary_id;
+    delete formatedUser.secondary_id;
   }
 
   const online = !!SocketManager.getSocketsByUserId(formatedUser.kitchat_user_id).length;
